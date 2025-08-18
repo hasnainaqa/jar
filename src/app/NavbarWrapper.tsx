14bd397b3bd3cbafe00187108565a688f4ea72ac
hasnainnaqa <muhammedhasnain033@gmail.com>
2025-08-18 16:12:47 +0500

    improvement

--- src/app/NavbarWrapper.tsx
+++ src/app/NavbarWrapper.tsx
@@ -5,7 +5,7 @@ import Navbar from "../components/ui/Navbar";
 
 export default function NavbarWrapper() {
   const pathname = usePathname();
-  const hideNavbar = pathname === "/signup"; // hide on signup page
+  const hideNavbar = pathname === "/signup" || pathname === "/profile-setup";
 
   return !hideNavbar ? <Navbar /> : null;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavbarWrapper from "./NavbarWrapper"; // ✅ client wrapper
+import NavbarWrapper from "./NavbarWrapper"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
